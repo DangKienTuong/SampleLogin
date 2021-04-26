43ce42fd9b87887d56096ec378062fc3c37766cc
Tuong <kientuong2000@gmail.com>
2021-04-26 15:53:36 +0700

    page factory

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,28 +1,35 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	WebDriver driver;
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
-	By username = By.name("uid");
-	By pass = By.name("password");
-	By loginBtn = By.name("btnLogin");
-	By logOutBtn = By.xpath("//*[text()='Log out']");
+	@FindBy(name="uid")
+    private WebElement weUserID;
+    @FindBy(name="password")
+    private WebElement wePassWord;
+    @FindBy(name="btnLogin")
+    private WebElement btnLogin;
+    @FindBy(xpath="//*[text()='Log out']")
+    private WebElement btnLogout;
 
 	public void Login(String userID, String passWord) {
-		driver.findElement(username).sendKeys(userID);
-		driver.findElement(pass).sendKeys(passWord);
-		driver.findElement(loginBtn).click();
+		weUserID.sendKeys(userID);
+		wePassWord.sendKeys(passWord);
+		btnLogin.click();
 	}
 	
 	public void Logout() {
-		driver.findElement(logOutBtn).click();
+		btnLogout.click();
 	}
 	
 }
